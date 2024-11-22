57b74d29e8cf080da4f7e543ea048a7626e408aa
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2024-11-22 10:38:59 +0500

    fix: navigation fixed

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -16,109 +16,111 @@ const Hero = () => {
   };
 
   return (
-    <header className="header">
-      <section className="site-hero" style={heroStyle}>
-        <h1 className="visually-hidden">RT Holdings</h1>
-        <div className="site__navigation">
-          <div className="site__container container">
-            <nav className="nav">
-              <ul className="nav__list">
-                <li className="nav__item">
-                  <NavLink
-                    className="nav__item-link nav__item-link--active"
-                    to="/"
-                  >
-                    Home
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/pages/about">
-                    About Us
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/pages/projects">
-                    Projects
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/pages/news">
-                    News
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/pages/sale">
-                    Sale
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/pages/services">
-                    Services
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/pages/contact">
-                    Contact Us
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
+    <>
+      <div className="site__navigation">
+        <div className="site__container container">
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <NavLink
+                  className="nav__item-link nav__item-link--active"
+                  to="/"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink className="nav__item-link" to="/pages/about">
+                  About Us
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink className="nav__item-link" to="/pages/projects">
+                  Projects
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink className="nav__item-link" to="/pages/news">
+                  News
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink className="nav__item-link" to="/pages/sale">
+                  Sale
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink className="nav__item-link" to="/pages/services">
+                  Services
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink className="nav__item-link" to="/pages/contact">
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
 
-            <div className="site__contact">
-              <ul className="site__medias">
-                <li className="site__meda">
-                  <a href="#" target="_blank">
-                    <IoLogoInstagram className="site__media-icon" />
-                  </a>
-                </li>
-                <li className="site__meda">
-                  <a href="#" target="_blank">
-                    <FaFacebook className="site__media-icon" />
-                  </a>
-                </li>
-                <li className="site__meda">
-                  <a href="#" target="_blank">
-                    <FaTelegramPlane className="site__media-icon" />
-                  </a>
-                </li>
-              </ul>
-              <a className="site__contact-link" href="#">
-                Contact Us
-              </a>
-            </div>
+          <div className="site__contact">
+            <ul className="site__medias">
+              <li className="site__meda">
+                <a href="#" target="_blank">
+                  <IoLogoInstagram className="site__media-icon" />
+                </a>
+              </li>
+              <li className="site__meda">
+                <a href="#" target="_blank">
+                  <FaFacebook className="site__media-icon" />
+                </a>
+              </li>
+              <li className="site__meda">
+                <a href="#" target="_blank">
+                  <FaTelegramPlane className="site__media-icon" />
+                </a>
+              </li>
+            </ul>
+            <a className="site__contact-link" href="#">
+              Contact Us
+            </a>
           </div>
         </div>
+      </div>
+      <header className="header">
+        <section className="site-hero" style={heroStyle}>
+          <h1 className="visually-hidden">RT Holdings</h1>
 
-        <div className="container">
-          <div className="site-hero__content">
-            <span className="site-hero__content-small">
-              Lorem ipsum dolor sit amet.
-            </span>
-            <h2 className="site-hero__content-title">
-              <span className="site-hero__conetent-among">Lorem</span> ipsum
-              dolor sit amet consectetur. Mauris eleifend dui
-            </h2>
+          <div className="container">
+            <div className="site-hero__content">
+              <span className="site-hero__content-small">
+                Lorem ipsum dolor sit amet.
+              </span>
+              <h2 className="site-hero__content-title">
+                <span className="site-hero__conetent-among">Lorem</span> ipsum
+                dolor sit amet consectetur. Mauris eleifend dui
+              </h2>
 
-            <p className="site-hero__content-text">
-              Lorem ipsum dolor sit amet consectetur. Velit sagittis nec
-              vulputate aliquet at suspendisse dui semper feugiat.
-            </p>
+              <p className="site-hero__content-text">
+                Lorem ipsum dolor sit amet consectetur. Velit sagittis nec
+                vulputate aliquet at suspendisse dui semper feugiat.
+              </p>
 
-            <div className="site-hero__buttons">
-              <Button
-                type="button"
-                className="site-hero__buttons-button site-hero__buttons-button--active"
-              >
-                Explore Our Services
-              </Button>
-              <Button className="site-hero__buttons-button" type="button">
-                Learn More About Us
-              </Button>
+              <div className="site-hero__buttons">
+                <Button
+                  type="button"
+                  className="site-hero__buttons-button site-hero__buttons-button--active"
+                >
+                  Explore Our Services
+                </Button>
+                <Button className="site-hero__buttons-button" type="button">
+                  Learn More About Us
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </header>
+        </section>
+      </header>
+    </>
   );
 };
 
