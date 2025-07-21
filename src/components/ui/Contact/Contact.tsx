7a03b56c58a5fb9b0c3fe385b9a8d6f91e9b95ab
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2025-07-22 01:23:06 +0500

    feat: Added form language data

--- src/components/ui/Contact/Contact.tsx
+++ src/components/ui/Contact/Contact.tsx
@@ -4,6 +4,8 @@ import "./style.css";
 import { ChangeEvent, useState } from "react";
 import { FaCircleCheck } from "@/assets/react-icons";
 import { Input, Textarea, Button } from "@/components";
+import { useSelector } from "react-redux";
+import { initialValuesTypes } from "@/context/reducer";
 
 const FormComponent = () => {
   const [form, setForm] = useState({
@@ -22,40 +24,183 @@ const FormComponent = () => {
     setForm({ ...form, [name]: value });
   };
 
+  const { appLang } = useSelector((state: initialValuesTypes) => state);
+
+  type ContactDataType = {
+    [key: string]: {
+      title: string;
+      data: { text: string }[];
+    };
+  };
+
+  const contactData: ContactDataType = {
+    uz: {
+      title: "Biz haqimizda",
+      data: [
+        {
+          text: "Malakali mutaxassislar",
+        },
+        {
+          text: "100% mijozlar mamnunligi",
+        },
+        {
+          text: "Shaffof narxlash",
+        },
+        {
+          text: "Sifatli ish",
+        },
+      ],
+    },
+    ru: {
+      title: "О нас",
+      data: [
+        {
+          text: "Квалифицированные специалисты",
+        },
+        {
+          text: "100% удовлетворенность клиентов",
+        },
+        {
+          text: "Прозрачное ценообразование",
+        },
+        {
+          text: "Качественная работа",
+        },
+      ],
+    },
+    en: {
+      title: "About us",
+      data: [
+        {
+          text: "Qualified specialists",
+        },
+        {
+          text: "100% customer satisfaction",
+        },
+        {
+          text: "Transparent pricing",
+        },
+        {
+          text: "Quality work",
+        },
+      ],
+    },
+    ko: {
+      title: "우리에 대해",
+      data: [
+        {
+          text: "전문가",
+        },
+        {
+          text: "100% 고객 만족",
+        },
+        {
+          text: "투명한 가격 책정",
+        },
+        {
+          text: "품질 작업",
+        },
+      ],
+    },
+  };
+
+  type offerDataType = {
+    [key: string]: {
+      title: string;
+      text: string;
+    };
+  };
+
+  const offerData: offerDataType = {
+    uz: {
+      title: "Taklif so‘rash",
+      text: "Mahsulotlar ustidan to‘liq nazorat bizga mijozlarimizga eng yuqori sifat, qulay narxlar va ishonchli xizmatlar taklif etish imkonini beradi. Jhontraktor kompaniyasi sifatida biz qilayotgan har bir ishimizdan faxrlanamiz.",
+    },
+    ru: {
+      title: "Запрос предложения",
+      text: "Полный контроль над продуктами позволяет нам предлагать нашим клиентам высочайшее качество, удобные цены и надежные услуги. Как компания Jhontraktor, мы гордимся каждой нашей работой.",
+    },
+    en: {
+      title: "Request an offer",
+      text: "Full control over products allows us to offer our clients the highest quality, convenient prices, and reliable services. As Jhontraktor company, we take pride in every work we do.",
+    },
+    ko: {
+      title: "제안 요청",
+      text: "제품에 대한 완전한 제어를 통해 고객에게 최고의 품질, 편리한 가격 및 신뢰할 수 있는 서비스를 제공할 수 있습니다. Jhontraktor 회사로서 우리는 우리가 하는 모든 일에 자부심을 가지고 있습니다.",
+    },
+  };
+
+  type formDataType = {
+    [key: string]: {
+      name: string;
+      email: string;
+      phone_number: string;
+      service: string;
+      send: string;
+    };
+  };
+
+  const formData: formDataType = {
+    uz: {
+      name: "Ism",
+      email: "Elektron pochta",
+      phone_number: "Telefon raqam",
+      service: "Xizmat turini lanlang",
+      send: "Murojaatni yuborish",
+    },
+    ru: {
+      name: "Имя",
+      email: "Электронная почта",
+      phone_number: "Номер телефона",
+      service: "Выберите тип услуги",
+      send: "Отправить запрос",
+    },
+    en: {
+      name: "Name",
+      email: "Email",
+      phone_number: "Phone number",
+      service: "Select service type",
+      send: "Send request",
+    },
+    ko: {
+      name: "이름",
+      email: "이메일",
+      phone_number: "전화번호",
+      service: "서비스 유형 선택",
+      send: "요청 보내기",
+    },
+  };
+
   return (
     <>
       <section className='contact'>
         <div className='container'>
           <div className='contact__content'>
-            <h2 className='contact__content-title'>Biz haqimizda</h2>
+            <h2 className='contact__content-title'>
+              {contactData[`${appLang}`]?.title}
+            </h2>
             <ul className='contact__list'>
-              <li className='contact__itme'>
-                <FaCircleCheck className='contact__item-icon' />
-                <p className='contact__item-text'>Malakali mutaxassislar</p>
-              </li>
-              <li className='contact__itme'>
-                <FaCircleCheck className='contact__item-icon' />
-                <p className='contact__item-text'>100% mijozlar mamnunligi</p>
-              </li>
-              <li className='contact__itme'>
-                <FaCircleCheck className='contact__item-icon' />
-                <p className='contact__item-text'>Shaffof narxlash</p>
-              </li>
-              <li className='contact__itme'>
-                <FaCircleCheck className='contact__item-icon' />
-                <p className='contact__item-text'>Sifatli ish</p>
-              </li>
+              {contactData[`${appLang}`]?.data.map((item, index) => {
+                return (
+                  <li
+                    className='contact__itme'
+                    key={index}
+                  >
+                    <FaCircleCheck className='contact__item-icon' />
+                    <p className='contact__item-text'>{item?.text}</p>
+                  </li>
+                );
+              })}
             </ul>
           </div>
 
           <div className='contact-request'>
             <div className='contact-request__content'>
-              <h3 className='contact-request__content-title'>Taklif so‘rash</h3>
+              <h3 className='contact-request__content-title'>
+                {offerData[`${appLang}`]?.title}
+              </h3>
               <p className='contact-request__content-text'>
-                Mahsulotlar ustidan to‘liq nazorat bizga mijozlarimizga eng
-                yuqori sifat, qulay narxlar va ishonchli xizmatlar taklif etish
-                imkonini beradi. Jhontraktor kompaniyasi sifatida biz qilayotgan
-                har bir ishimizdan faxrlanamiz.
+                {offerData[`${appLang}`]?.text}
               </p>
             </div>
 
@@ -66,8 +211,8 @@ const FormComponent = () => {
                   type='text'
                   name='name'
                   value={form.name}
-                  placeholder='Ism'
                   onChange={changeInput}
+                  placeholder={formData[`${appLang}`].name}
                   className='contact-request-form__box-input'
                 />
                 <Input
@@ -76,7 +221,7 @@ const FormComponent = () => {
                   name='email'
                   value={form.email}
                   onChange={changeInput}
-                  placeholder='Elektron pochta'
+                  placeholder={formData[`${appLang}`]?.email}
                   className='contact-request-form__box-input'
                 />
               </label>
@@ -87,7 +232,7 @@ const FormComponent = () => {
                   name='phone_number'
                   onChange={changeInput}
                   value={form.phone_number}
-                  placeholder='Telefon raqam'
+                  placeholder={formData[`${appLang}`]?.phone_number}
                   className='contact-request-form__box-input'
                 />
                 <select
@@ -97,8 +242,8 @@ const FormComponent = () => {
                   onChange={changeInput}
                   className='contact-request-form__box-input select'
                 >
-                  <option disabled>Xizmat turini tanlang</option>
-                  <option value='value 1'>Qiymat 1</option>
+                  <option disabled>{formData[`${appLang}`]?.service}</option>
+                  {/* <option value='value 1'>Qiymat 1</option> */}
                 </select>
               </label>
               <Textarea
@@ -111,7 +256,7 @@ const FormComponent = () => {
                 type='submit'
                 className='contact-request-form__box-submit'
               >
-                Murojaatni yuborish
+                {formData[`${appLang}`]?.send}
               </Button>
             </form>
           </div>
