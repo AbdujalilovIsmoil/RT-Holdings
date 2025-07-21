8637fcc0a8d0676f0aa395cc27b43788cf379ffd
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2025-07-22 01:56:22 +0500

    feat: Added language data

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { initialValuesTypes } from "@/context/reducer";
 import "./not-found.css";
 import Image from "next/image";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Error = () => {
   useEffect(() => {
@@ -10,6 +12,38 @@ const Error = () => {
       top: 0,
     });
   }, []);
+
+  const { appLang } = useSelector((state: initialValuesTypes) => state);
+
+  interface NotFoundData {
+    [key: string]: {
+      title: string;
+      description: string;
+    };
+  }
+
+  const notFoundData: NotFoundData = {
+    uz: {
+      title: "404, Sahifa topilmadi",
+      description:
+        "Biror narsa noto'g'ri ketdi. Sizning so'rovingiz topilmadi. Havola buzilgan yoki sahifa o'chirilgan ko'rinadi.",
+    },
+    ru: {
+      title: "404, Страница не найдена",
+      description:
+        "Что-то пошло не так. Похоже, что ваш запрос не найден. Возможно, ссылка повреждена или страница удалена.",
+    },
+    en: {
+      title: "404, Page not found",
+      description:
+        "Something went wrong. It looks like your request could not be found. It looks like the link is broken or the page is removed.",
+    },
+    ko: {
+      title: "404, 페이지를 찾을 수 없음",
+      description:
+        "문제가 발생했습니다. 요청하신 내용을 찾을 수 없습니다. 링크가 깨졌거나 페이지가 삭제된 것 같습니다.",
+    },
+  };
 
   return (
     <section className='not-found'>
@@ -22,11 +56,12 @@ const Error = () => {
             src='/images/png/not-found.png'
             className='not-found__content-image'
           />
-          <h1 className='not-found__content-title'>404, Page not founds</h1>
+          <h1 className='not-found__content-title'>
+            {notFoundData[`${appLang}`].title}
+          </h1>
 
           <p className='not-found__content-text'>
-            Something went wrong. It’s look that your requested could not be
-            found. It’s look like the link is broken or the page is removed.
+            {notFoundData[`${appLang}`].description}
           </p>
         </div>
       </div>
